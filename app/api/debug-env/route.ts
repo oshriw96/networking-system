8220de371ca72da38fcad5cc459df67eb6e87e32
oshriw96 <oshriw96@gmail.com>
2026-03-18 15:45:45 +0200

    Remove debug endpoint

--- app/api/debug-env/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({
-    has_service_role: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-    has_admin_password: !!process.env.ADMIN_PASSWORD,
-    service_role_length: process.env.SUPABASE_SERVICE_ROLE_KEY?.length || 0,
-  })
-}
